1bf4949314a18f1cc6ffd60ab21b79c82832acb2
wpverdecia <wpverdecia@unicesar.edu.co>
2026-02-28 18:49:45 -0500

    feat:crear estructura base de Main con ArrayLists

---
+++ src/main/java/com/mycompany/sistemaacademico/Main.java
@@ -0,0 +1,24 @@
+package com.mycompany.sistemaacademico;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    
+    private ArrayList<String> estudiantes;
+    private ArrayList<String> asignaturas;
+    private ArrayList<String> notas;
+
+    public Main() {
+        this.estudiantes = new ArrayList<>();
+        this.asignaturas = new ArrayList<>();
+        this.notas = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}
+
+    
