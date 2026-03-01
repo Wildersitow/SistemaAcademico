07cf69590d7e627d6beec6a4784b23c628b870f9
Leonel Garrido <legarrido@unicesar.edu.co>
2026-03-01 15:00:46 -0500

    feat: implementar CREATE - registrar asignatura

--- src/main/java/com/mycompany/sistemaacademico/SistemaAcademico.java
+++ src/main/java/com/mycompany/sistemaacademico/SistemaAcademico.java
@@ -8,9 +8,17 @@ package com.mycompany.sistemaacademico;
  *
  * @author HP
  */
+import java.util.ArrayList;
+import java.util.Scanner;
 public class SistemaAcademico {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+   static ArrayList<String> asignaturas = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void registrarAsignatura() {
+        System.out.print("Ingrese nombre de la asignatura: ");
+        String nombre = scanner.nextLine();
+        asignaturas.add(nombre);
+        System.out.println("Asignatura registrada correctamente.");
     }
 }
