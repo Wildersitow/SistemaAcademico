a252eaf781a44cc25bc805b9b39d913a902d1686
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-03-01 16:34:17 -0500

    feat: agregar toString a clase Asignatura

--- src/main/java/com/mycompany/sistemaacademico/SistemaAcademico.java
+++ src/main/java/com/mycompany/sistemaacademico/SistemaAcademico.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.sistemaacademico;
 
-/**
- *
- * @author HP
- */
 public class SistemaAcademico {
 
     public static void main(String[] args) {
