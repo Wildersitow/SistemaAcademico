5e593beb02724e03ae5d2ebdf23d2830564f9c06
wpverdecia <wpverdecia@unicesar.edu.co>
2026-03-03 17:16:57 -0500

    feat: arreglo de errores en menu principal

--- src/main/java/com/mycompany/sistemaacademico/Estudiante.java
+++ src/main/java/com/mycompany/sistemaacademico/Estudiante.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.sistemaacademico;
 
-/**
- *
- * @author Athan
- */
+
 public class Estudiante {
     private String nombre;
+    private String apellido;
     private int edad;
+    private int semestre;
     private String codigo;
-public Estudiante(String nombre, int edad, String codigo) {
+public Estudiante(String nombre,String apellido, int edad, int semestre, String codigo) {
         this.nombre = nombre;
+        this.apellido = apellido;
         this.edad = edad;
+        this.semestre = semestre;
         this.codigo = codigo;
     }
 public String getNombre() {
@@ -40,12 +37,32 @@ public String getNombre() {
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+    
+    
     
     @Override
 public String toString() {
     return "Estudiante{" +
-            "nombre='" + nombre + '\'' +
-            ", edad=" + edad +
+            "nombre=" + nombre + '\'' +
+            ", edad=" + edad +"apellido="
+            + apellido + '\'' +
+            ", semestre=" + semestre +
             ", codigo='" + codigo + '\'' +
             '}';
 }
--- src/main/java/com/mycompany/sistemaacademico/Main.java
+++ src/main/java/com/mycompany/sistemaacademico/Main.java
@@ -85,7 +85,7 @@ public class Main {
         System.out.print("Ingrese el semestre: ");
         int semestre = scanner.nextInt();
         scanner.nextLine();
-        Estudiante e = new Estudiante(codigo, nombre, apellido, edad, semestre);
+        Estudiante e = new Estudiante(nombre,apellido, edad,semestre, codigo);
         estudiantes.add(e);
         System.out.println("Estudiante registrado correctamente.");
     }
@@ -164,6 +164,54 @@ public class Main {
             System.out.println("Estudiante no encontrado.");
         }
     }
+    
+    public static void registrarAsignatura() {
+        System.out.print("Ingrese nombre de la asignatura: ");
+        String nombre = scanner.nextLine();
+        asignaturas.add(nombre);
+        System.out.println("Asignatura registrada correctamente.");
+    }
+    public static void listarAsignaturas() {
+    for (String asignatura : asignaturas) {
+        System.out.println(asignatura);
+    }
+}
+
+public static void buscarAsignatura() {
+    System.out.print("Ingrese nombre a buscar: ");
+    String nombre = scanner.nextLine();
+
+    if (asignaturas.contains(nombre)) {
+        System.out.println("Asignatura encontrada.");
+    } else {
+        System.out.println("No existe.");
+    }
+}
+public static void actualizarAsignatura() {
+    System.out.print("Nombre actual: ");
+    String actual = scanner.nextLine();
+
+    int index = asignaturas.indexOf(actual);
+
+    if (index != -1) {
+        System.out.print("Nuevo nombre: ");
+        String nuevo = scanner.nextLine();
+        asignaturas.set(index, nuevo);
+        System.out.println("Actualizada correctamente.");
+    } else {
+        System.out.println("No encontrada.");
+    }
+}
+public static void eliminarAsignatura() {
+    System.out.print("Nombre a eliminar: ");
+    String nombre = scanner.nextLine();
+
+    if (asignaturas.remove(nombre)) {
+        System.out.println("Eliminada correctamente.");
+    } else {
+        System.out.println("No encontrada.");
+    }
+}
     
     static void registrarNota(ArrayList<Nota> notas, ArrayList<Estudiante> estudiantes, ArrayList<Asignatura> asignaturas, Scanner sc) {
     System.out.print("Código del estudiante: ");
